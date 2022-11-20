f80613e1560c4b8b9ecc20b79382cc7ea268ee75
Kevin Mallari <kevinmallari62@gmail.com>
2022-11-20 15:33:03 +0800

    fix mail apii

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -2,7 +2,6 @@ import sendgrid from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
-console.log("YOYOYOY", process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
   const { email, name, message } = req.body;
@@ -47,7 +46,6 @@ async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
       });
   } catch (error) {
     console.log(error);
-    return res.status(error.statusCode || 500).json({ error: error.message });
   }
 
   return res.status(200).json({ error: "" });
